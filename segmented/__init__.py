adc1c1920ba5f1005906b80a090494766efbd1f4
Christian Luhmann <cluhmann@gmail.com>
2022-03-06 22:07:09 -0500

    initial bayesianyesian parametric changepoint added

--- segmented/__init__.py
+++ segmented/__init__.py
@@ -3,4 +3,5 @@
 from .demo import demo
 from .frequentist import nonparametric
 from .frequentist import segmented
-from .bayes import bayes
+from .bayes import bayes_nonparametric
+from .bayes import bayes_parametric
